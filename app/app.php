8791c43e82d86fc8bc4872e7f1e82fa51dd097ca
Claire <claireserra37@gmail.com>
2017-02-16 17:56:35 +0100

    ajout tinymce, debug dans composer

--- app/app.php
+++ app/app.php
@@ -3,6 +3,7 @@
 use Symfony\Component\Debug\ErrorHandler;
 use Symfony\Component\Debug\ExceptionHandler;
 
+
 // Register global error and exception handlers
 ErrorHandler::register();
 ExceptionHandler::register();
@@ -37,7 +38,8 @@ $app->register(new Silex\Provider\SecurityServiceProvider(), array(
             'logout' => true,
             'form' => array('login_path' => '/login', 'check_path' => '/login_check'),
             'users' => function () use ($app) {
-                return new writerblog\DAO\UserDAO($app['db']);
+                $u = new writerblog\DAO\UserDAO($app['db']);
+                return $u;
             },
         ),
     ),
